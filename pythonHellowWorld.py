84788211585a64523a8e2a1aeb2028dd48ad7e1c
511517 <511517@PC354106.cts.com>
2018-08-10 15:07:42 +0530

    new changes

--- pythonHellowWorld.py
+++ pythonHellowWorld.py
@@ -1,11 +1,35 @@
-from bottle import route, run
+from bottle import route, run ,template ,static_file,view
+import sys
+import os
+
 
 @route('/hello')
 def hello():
+ 
     return "Hello World!"
 @route('/switch/<number>/<action>')
 def switchProcessing(number='number',action='action'):
 
-   return  'the switch '+number+' undergoes action '+action
+ 
+ return  'the switch '+number+' undergoes action '+action
 
+ 
+
+@route('/')
+@view('index')
+def index():
+ 
+    return { 'get_url': '' } 
+	
+@route('/')
+@view('index')
+def js():
+ 
+    return { 'get_url': '' } 
+	
+	
+@route('/static/:path#.+#', name='static')
+def static(path):
+    return static_file(path, root='static')
+	
 run(host='localhost', port=8080, debug=True)
---
+++ views/index.tpl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8" />
+        <script type="text/javascript" src="js/main.js" charset="utf-8"></script>
+    </head>
+    <body>
+	sdfsdfsdfsdfsdfsdfsdfsfsdfsdfsdf
+    </body>
+</html>
