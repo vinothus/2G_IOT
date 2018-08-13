39670048197e08059dcc5e3b4f5f1a5c925d54ac
511517 <511517@PC354106.cts.com>
2018-08-13 16:52:17 +0530

    commit times

--- pythonHellowWorld.py
+++ pythonHellowWorld.py
@@ -7,8 +7,8 @@ import sys
 import bottle
 from bottle_log import LoggingPlugin
 
-app = bottle.Bottle()
-app.install(LoggingPlugin(app.config))
+#app = bottle.Bottle()
+#app.install(LoggingPlugin(app.config))
 #path = 'C:\Users\511517\Desktop\bottle'
 #sys.path.append(r"C:\Users\511517\Desktop\bottle")
 def printme( str ):
@@ -25,9 +25,9 @@ def printme( str ):
 
    spi.close()
    return	
-@app.get('/hello')
+
 @route('/hello')
-def hello(logger):
+def hello():
 # path = 'C:\Users\511517\Desktop\bottle';
 #os.environ['PATH'] += ':'+path;
 #response.charset = 'ISO-8859-15'
@@ -36,9 +36,9 @@ def hello(logger):
     logger.warning('This is only a test')
     return "Hello World!"+ os.getcwd() + str(sys.path)[1:-1]  +os.environ['PATH'] +os.path.realpath(__file__) +os.path.dirname(sys.argv[0])
 	
-@app.get('/switch/<number>/<action>')
+
 @route('/switch/<number>/<action>')
-def switchProcessing(logger,number='number',action='action'):
+def switchProcessing(number='number',action='action'):
 
  logger.warning('the switch '+number+' undergoes action '+action)
  return  'the switch '+number+' undergoes action '+action
@@ -57,18 +57,18 @@ def js():
  
     return { 'get_url': '' } 
 	
-@app.get('/static/:path#.+#')	
+
 @route('/static/:path#.+#', name='static')
-def static(logger,path):
+def static(path):
     logger.warning('static')
     return static_file(path, root='static')
-@app.get('/counter')	
+
 @route('/counter')
-def counter(logger):
+def counter():
     count = int( request.cookies.get('counter', '0') )
     count += 1
     response.set_cookie('counter', str(count))
     return 'You visited this page %d times' % count
 
 
-run(app,host='0.0.0.0', port=8080, debug=True)
+run(host='0.0.0.0', port=8080, debug=True)
