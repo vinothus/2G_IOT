638079ac3d867fbb90db134261589b5afac5b749
vinoth <vinoth@vinoth-PC>
2018-08-14 20:51:57 +0530

    error correction

--- StartSmartHome.sh
+++ StartSmartHome.sh
@@ -1,1 +1,2 @@
-./data/data/org.qpython.qpy3/files/bin/qpython-android5.sh "/storage/emulated/0/Android/data/com.manichord.mgit/files/repo/2G_IOT/pythonHelloWorld.py"
+cd /data/data/org.qpython.qpy3/files/bin
+./qpython-android5.sh "/storage/emulated/0/Android/data/com.manichord.mgit/files/repo/2G_IOT/pythonHelloWorld.py"
