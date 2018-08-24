8ba16614877606848be8aae9621e86ec203a4bf9
vinoth <vinoth@vinoth-PC>
2018-08-25 02:43:42 +0530

    room icon dynamically added

--- pythonHellowWorld.py
+++ pythonHellowWorld.py
@@ -44,7 +44,7 @@ def getValOut(pinnum):
 def initDB():
   conn = sqlite3.connect('HomeAutomation.db')
   conn.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, name char(100) NOT NULL, password char(100) NOT NULL , email char(100), phoneno char(12) ,address char(100))")
-  conn.execute("CREATE TABLE IF NOT EXISTS rooms (id INTEGER PRIMARY KEY, roomname char(100) NOT NULL, roomdesc char(100) NOT NULL )")
+  conn.execute("CREATE TABLE IF NOT EXISTS rooms (id INTEGER PRIMARY KEY, roomname char(100) NOT NULL, roomdesc char(100) NOT NULL ,uiicon char(100))")
   conn.execute("CREATE TABLE IF NOT EXISTS port (id INTEGER PRIMARY KEY, portnamename char(100) NOT NULL, portdesc char(100) NOT NULL,porttype char(100) NOT NULL ,porthdid char(100) NOT NULL)")
   conn.execute("CREATE TABLE IF NOT EXISTS households (id INTEGER PRIMARY KEY,roomid INTEGER, householdname char(100) NOT NULL, householddesc char(100) NOT NULL,householdport INTEGER NOT NULL ,uiicon char(100))")
   conn.execute("INSERT OR REPLACE INTO port(id, portnamename,portdesc,porttype,porthdid) VALUES(1, 'GPIO56','General purpose I/O port','GPIO','56')")
@@ -220,10 +220,10 @@ def getRooms():
 def makeRoom():
     roonname=request.query['roonname']
     roomdesc=request.query['roomdesc']
-    
+    uiicon=request.query['uiicon']
     conn = sqlite3.connect('HomeAutomation.db')
     c = conn.cursor()
-    c.execute("INSERT OR REPLACE INTO rooms(roomname,roomdesc) VALUES (?,?)", (roonname,roomdesc))
+    c.execute("INSERT OR REPLACE INTO rooms(roomname,roomdesc,uiicon) VALUES (?,?,?)", (roonname,roomdesc,uiicon))
     new_id = c.lastrowid
 
     conn.commit()    
