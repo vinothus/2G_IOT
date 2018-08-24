224023f505d4d04dfbdaf6d9e97c95c99ec939ad
511517 <511517@PC354106.cts.com>
2018-08-24 14:28:30 +0530

    commit delete table

--- pythonHellowWorld.py
+++ pythonHellowWorld.py
@@ -46,7 +46,7 @@ def initDB():
   conn.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, name char(100) NOT NULL, password char(100) NOT NULL , email char(100), phoneno char(12) ,address char(100))")
   conn.execute("CREATE TABLE IF NOT EXISTS rooms (id INTEGER PRIMARY KEY, roomname char(100) NOT NULL, roomdesc char(100) NOT NULL )")
   conn.execute("CREATE TABLE IF NOT EXISTS port (id INTEGER PRIMARY KEY, portnamename char(100) NOT NULL, portdesc char(100) NOT NULL,porttype char(100) NOT NULL ,porthdid char(100) NOT NULL)")
-  conn.execute("CREATE TABLE IF NOT EXISTS households (id INTEGER PRIMARY KEY,roomid INTEGER, householdname char(100) NOT NULL, householddesc char(100) NOT NULL,householdport char(100) NOT NULL)")
+  conn.execute("CREATE TABLE IF NOT EXISTS households (id INTEGER PRIMARY KEY,roomid INTEGER, householdname char(100) NOT NULL, householddesc char(100) NOT NULL,householdport INTEGER NOT NULL)")
   conn.execute("INSERT OR REPLACE INTO port(id, portnamename,portdesc,porttype,porthdid) VALUES(1, 'GPIO56','General purpose I/O port','GPIO','56')")
   conn.execute("INSERT OR REPLACE INTO port(id, portnamename,portdesc,porttype,porthdid) VALUES(2, 'GPIO122','General purpose I/O port','GPIO','122')")
   conn.execute("INSERT OR REPLACE INTO port(id, portnamename,portdesc,porttype,porthdid) VALUES(3, 'GPIO123','General purpose I/O port','GPIO','123')")
@@ -57,7 +57,12 @@ def initDB():
   conn.execute("INSERT OR REPLACE INTO port(id, portnamename,portdesc,porttype,porthdid) VALUES(8, 'GPIO121','General purpose I/O port','GPIO','121')")  
   conn.commit()
   print('database initiated')
-  
+def deleteTable(tableName):
+  conn = sqlite3.connect('HomeAutomation.db')  
+  conn.execute("DROP TABLE "+tableName)
+  print('Dropped Table : '+tableName)
+  conn.commit()
+  return True
 def _init_():
   try:
      print('init script')
@@ -67,6 +72,10 @@ def _init_():
 _init_() 
 initDB()
 
+@route('/dropTable')
+def dropTable():
+    tableName=request.query['tableName']
+    return deleteTable(tableName)
 @route('/hello')
 def hello():
 # path = 'C:\Users\511517\Desktop\bottle';
@@ -95,7 +104,15 @@ def switchProcessing(number='number'):
 def index():
    # logger.warning('index')
     return { 'get_url': '' } 
-	
+@route('/houseHolds')
+@view('houseHolds')
+def houseHolds():
+    roomid=request.query['roomid']
+    conn = sqlite3.connect('HomeAutomation.db')
+    c = conn.cursor()
+    c.execute("SELECT h.* FROM households h join  port p where h.roomid = ? and p.id=h.householdport ", (roomid,))
+    result = c.fetchall()
+    return {'roomname':'BathRoom','roomid':roomid ,'result':result}	
 @route('/')
 @view('index')
 def js():
